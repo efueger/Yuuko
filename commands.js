179d79045e31031086aaec65554f0c0742320c86
Geo1088 <georgej1088@gmail.com>
2016-08-23 10:30:03 -0400

    Basic NPM lookup command

--- commands.js
+++ commands.js
@@ -62,6 +62,23 @@ commands.request = {
     }
 }
 
+commands.npm = {
+    name: 'npm',
+    desc: 'Looks up a package on NPM.',
+    usage: '<package>',
+    process: (c, msg, args) => {
+        c.reply(msg, `Looking up package \`${args}\`, please wait...`).then(reply => {
+            request(`https://www.npmjs.com/package/${args}`, (err, res) => {
+                if (res.statusCode === 200) {
+                    c.editMessage(reply.channel.id, reply.id, `https://www.npmjs.com/package/${args}`)
+                } else {
+                    c.editMessage(reply.channel.id, reply.id, 'Package not found')
+                }
+            })
+        })
+    }
+}
+
 commands.prefix = {
     name: 'prefix',
     desc: 'Changes the server prefix',
